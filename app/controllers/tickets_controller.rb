e032840b7e4c5efdcbb4d106b912688b1097395f
Ron Phillips <rphillips@summitengineer.net>
2011-10-19 11:11:51 -0400

    11_1 Users can tag tickets upon creation

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -16,9 +16,9 @@ class TicketsController < ApplicationController
     @ticket.assets.build
   end
   def create
-    #p params
     @ticket = @project.tickets.build(params[:ticket].merge!(:user => current_user))
     if @ticket.save
+      @ticket.tag!(params[:tags])
       flash[:notice] = "Ticket has been created."
       redirect_to [@project, @ticket]
     else
